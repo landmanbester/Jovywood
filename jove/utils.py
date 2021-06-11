3142be9915a4a5021dcefe48b3829c04152e6803
landmanbester <lbester@ska.ac.za>
2021-06-11 13:31:29 +0200

    Write out interp cube instead of pngs

--- jove/utils.py
+++ jove/utils.py
@@ -207,3 +207,44 @@ def abs_diff(x, xp):
         xD[i] = np.tile(x[:, i], (Np, 1)).T
         xpD[i] = np.tile(xp[:, i], (N, 1))
     return np.linalg.norm(xD - xpD, axis=0)
+
+def test_gpr():
+    N = 100
+    t = np.sort(np.random.random(N))
+    Np = 250
+    tp = np.linspace(0, 1, Np)
+    f = np.cos(10*t)
+    Sigma = 1e-3 * (1 + np.random.random(N))
+    y = f + np.sqrt(Sigma) * np.random.randn(N)
+
+    sigmaf0 = np.std(y)
+    theta0 = np.array([np.std(y), 1.0, 1.0])
+
+
+    xxsq = abs_diff(t, t)
+
+
+    from time import time
+    ti = time()
+    theta, fval, dinfo = fmin(dZdtheta, theta0, args=(xxsq, y, Sigma), approx_grad=False,
+                              bounds=((1e-3, None), (1e-3, None), (1e-3, None)))
+                              # factr=1e9, pgtol=1e-4)
+    print(time() - ti)
+    print(theta)
+    print(fval)
+    print(dinfo)
+
+    # theta[1] = 2*theta[1]
+
+    # K = theta[0]**2*np.exp(-xxsq/(2*theta[1]**2))
+    # Ky = K + np.diag(Sigma) * 2 * theta[2]**2
+    # xxpsq = abs_diff(tp, t)
+    # Kp = theta[0]**2*np.exp(-xxpsq/(2*theta[1]**2))
+    # fp = Kp.dot(np.linalg.solve(Ky, y))
+
+    # import matplotlib.pyplot as plt
+
+    # plt.plot(t, f, 'k')
+    # plt.plot(tp, fp, 'b')
+    # plt.errorbar(t, y, np.sqrt(Sigma)*theta[2], fmt='xr')
+    # plt.show()
--- jove/workers/interp.py
+++ jove/workers/interp.py
@@ -13,7 +13,9 @@ log = pyscilog.get_logger('INTERP')
               help="Path to hypers.zarr")
 @click.option("-o", "--outfile", type=str, default='pfb',
               help='Base name of output file.')
-@click.option("-pc", "--pix_chunks", type=int, default=50,
+@click.option("-pc", "--pix-chunks", type=int, default=50,
+              help='Pixel chunks')
+@click.option("-poc", "--pix-out-chunks", type=int, default=50,
               help='Pixel chunks')
 @click.option('-nto', "--ntime-out", type=int,
               help="Number of output times")
@@ -43,6 +45,7 @@ def interp(**kw):
     os.environ["VECLIB_MAXIMUM_THREADS"] = str(1)
     os.environ["NUMBA_NUM_THREADS"] = str(1)
     import numpy as np
+    from scipy.interpolate
     import xarray as xr
     from jove.utils import abs_diff
     import dask.array as da
@@ -52,132 +55,90 @@ def interp(**kw):
     from glob import glob
     import matplotlib.pyplot as plt
     from matplotlib.colors import LogNorm
-
+    from scipy.interpolate import interp1d
     from jove.utils import interp_pix
 
-    from multiprocessing.pool import Pool
-    with Pool(processes=args.nthreads) as pool:
-        dask.config.set(pool=pool)
+    from multiprocessing.pool import ThreadPool
+    dask.config.set(pool=ThreadPool(processes=args.nthreads))
 
-        Din = xr.open_dataset(args.data, chunks={'time':-1,
-                                                'nx':args.pix_chunks,
-                                                'ny':args.pix_chunks},
-                                                engine='zarr')
+    Din = xr.open_dataset(args.data, chunks={'time':-1,
+                                            'nx':args.pix_chunks,
+                                            'ny':args.pix_chunks},
+                                            engine='zarr')
 
-        image = Din.image.data
-        rmss = Din.rmss.data
-        times = Din.times.data
-        ras = Din.ras.data
-        decs = Din.decs.data
+    image = Din.image.data
+    rmss = Din.rmss.data
+    times = Din.times.data
+    ras = Din.ras.data
+    decs = Din.decs.data
 
-        thetas = xr.open_dataset(args.hypers, chunks={'time':-1,
-                                                      'nx':args.pix_chunks,
-                                                      'ny':args.pix_chunks},
-                                                      engine='zarr').thetas.data
+    thetas = xr.open_dataset(args.hypers, chunks={'time':-1,
+                                                    'nx':args.pix_chunks,
+                                                    'ny':args.pix_chunks},
+                                                    engine='zarr').thetas.data
 
-        ntime, nx, ny = image.shape
+    ntime, nx, ny = image.shape
 
-        # normalise to between 0 and 1
-        t = times - times.min()
-        t = t/t.max()
+    # normalise to between 0 and 1
+    tmin = times.min()
+    tmax = times.max()
+    t = times - times.min()
+    t = t/t.max()
 
-        # precompute abs diffs
-        xxsq = abs_diff(t, t)
-        tp = np.linspace(0, 1, args.ntime_out)
-        xxpsq = abs_diff(tp, t)
+    raso = interp1d(t, ras, kind='cubic', assume_sorted=True)
+    decso = interp1d(t, decs, kind='cubic', assume_sorted=True)
 
-        Sigma = rmss**2
-        sigman0 = 1.0
-
-        thetas = da.blockwise(
-            interp_pix, 'txy',
-            thetas, 'pxy',
-            image, 'txy',
-            xxsq, None,
-            xxpsq, None,
-            Sigma, None,
-            args.oversmooth, None,
-            align_arrays=False,
-            dtype=image.dtype
-        )
-
-        print('Fitting')
-        with ProgressBar():
-            thetas = dask.compute(thetas)[0]  # scheduler='processes'
-
-        image_out = da.from_array(thetas, chunks = (3, args.pix_chunks, args.pix_chunks), name=False)
-
-        data_vars = {'theta':(('three', 'nx', 'ny'), image_out)}
-        coords = {'times': (('time',), tp)}
-
-        Dout = xr.Dataset(data_vars, coords)
-        Dout.to_zarr(args.outfile + '_hypers.zarr', mode='w')
-
-    print('2png')
-    imgs = []
-    for i in range(args.ntime_out):
-        plt.figure()
-        plt.imshow(imagep[i].T, cmap='inferno', vmin=1e-6, vmax=0.15, origin='lower')
-        plt.title(str(i))
-        plt.savefig(args.outfile + str(i) + '.png', dpi=300)
-        imgs.append(args.outfile + str(i) + '.png')
-        plt.close()
-
-    print('2gif')
-    frames = []
-    for i in imgs:
-        new_frame = Image.open(i)
-        frames.append(new_frame)
-
-    frames[0].save(args.outfile + '.gif', format='GIF',
-                append_images=frames[1:],
-                save_all=True,
-                duration=args.duration*1000/args.ntime_out, loop=0)
-
-
-
-
-def test_gpr():
-    N = 100
-    t = np.sort(np.random.random(N))
-    Np = 250
-    tp = np.linspace(0, 1, Np)
-    f = np.cos(10*t)
-    Sigma = 1e-3 * (1 + np.random.random(N))
-    y = f + np.sqrt(Sigma) * np.random.randn(N)
-
-    sigmaf0 = np.std(y)
-    theta0 = np.array([np.std(y), 1.0, 1.0])
-
-
+    # precompute abs diffs
     xxsq = abs_diff(t, t)
+    tp = np.linspace(0, 1, args.ntime_out)
+    xxpsq = abs_diff(tp, t)
 
+    Sigma = rmss**2
 
-    from time import time
-    ti = time()
-    theta, fval, dinfo = fmin(dZdtheta, theta0, args=(xxsq, y, Sigma), approx_grad=False,
-                              bounds=((1e-3, None), (1e-3, None), (1e-3, None)))
-                              # factr=1e9, pgtol=1e-4)
-    print(time() - ti)
-    print(theta)
-    print(fval)
-    print(dinfo)
+    image_out = da.blockwise(
+        interp_pix, 'txy',
+        thetas, 'pxy',
+        image, 'txy',
+        xxsq, None,
+        xxpsq, None,
+        Sigma, None,
+        args.oversmooth, None,
+        align_arrays=False,
+        dtype=image.dtype
+    )
 
-    # theta[1] = 2*theta[1]
+    tout = da.from_array(tmin + tp*tmax, chunks=1)
+    rasout = da.from_array(raso(tp), chunks=1)
+    decsout = da.from_array(decso(tp), chunks=1)
 
-    # K = theta[0]**2*np.exp(-xxsq/(2*theta[1]**2))
-    # Ky = K + np.diag(Sigma) * 2 * theta[2]**2
-    # xxpsq = abs_diff(tp, t)
-    # Kp = theta[0]**2*np.exp(-xxpsq/(2*theta[1]**2))
-    # fp = Kp.dot(np.linalg.solve(Ky, y))
+    data_vars = {'image':(('time', 'nx', 'ny'),
+                 image_out.rechunk({'time':1,
+                                    'nx':args.pix_out_chunks,
+                                    'ny':args.pix_out_chunks}))}
+    coords = {'times': (('time',), tout),
+                'ras': (('time'), rasout)),
+                'decs': (('time'), decsout)}
 
-    # import matplotlib.pyplot as plt
+    Dout = xr.Dataset(data_vars, coords)
+    Dout.to_zarr(args.outfile + '_hypers.zarr', mode='w', compute=True)
 
-    # plt.plot(t, f, 'k')
-    # plt.plot(tp, fp, 'b')
-    # plt.errorbar(t, y, np.sqrt(Sigma)*theta[2], fmt='xr')
-    # plt.show()
+    # print('2png')
+    # imgs = []
+    # for i in range(args.ntime_out):
+    #     plt.figure()
+    #     plt.imshow(imagep[i].T, cmap='inferno', vmin=1e-6, vmax=0.15, origin='lower')
+    #     plt.title(str(i))
+    #     plt.savefig(args.outfile + str(i) + '.png', dpi=300)
+    #     imgs.append(args.outfile + str(i) + '.png')
+    #     plt.close()
 
+    # print('2gif')
+    # frames = []
+    # for i in imgs:
+    #     new_frame = Image.open(i)
+    #     frames.append(new_frame)
 
-
-# 24 * 5 time stamps
+    # frames[0].save(args.outfile + '.gif', format='GIF',
+    #             append_images=frames[1:],
+    #             save_all=True,
+    #             duration=args.duration*1000/args.ntime_out, loop=0)
